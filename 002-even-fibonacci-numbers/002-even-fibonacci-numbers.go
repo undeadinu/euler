71486bf4dbcacd48bc043684df8b2fb02761bfce
Nikita Voloboev <nikita.voloboev@icloud.com>
2017-08-24 18:25:18 +0200

    trying to test

--- 002-even-fibonacci-numbers/002-even-fibonacci-numbers.go
+++ 002-even-fibonacci-numbers/002-even-fibonacci-numbers.go
@@ -1,14 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
+
+// timeTrack calculates time a function takes to run in seconds
+func timeTrack(start time.Time, name string) {
+	elapsed := time.Since(start).Seconds()
+	fmt.Printf("%s took %.8f seconds", name, elapsed)
+	fmt.Println()
+}
 
 // fibo calculates the sum of even-valued terms in a fibonacci sequence until some number
 func fibo(limit int) int {
-	// variables:  a, b = 1, c, sum = 0
 	a, b, c, sum := 1, 1, 0, 0
 
 	for {
 
+		// break once we reach our limit
 		if c > limit {
 			break
 		}
@@ -29,5 +39,5 @@ func fibo(limit int) int {
 }
 
 func main() {
-	fmt.Println(fibo(4000000))
+	fmt.Println(fibo(40000))
 }
---
+++ 002-even-fibonacci-numbers/002-even-fibonacci-numbers_test.go
@@ -0,0 +1,3 @@
+func BenchmarkFibo(*testing.B)  {
+
+}
